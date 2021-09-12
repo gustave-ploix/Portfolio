2301dfb3144ea91712f92ad12c490c17854f4862
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-12 19:47:42 +0200

    cleaned the scss and the JS.

--- js/landing.js
+++ js/landing.js
@@ -6,28 +6,28 @@ const btn = document.getElementsByClassName('rond')[0];
 const main = document.querySelector('main');
 
 
-// redirect to the home page
-const redirect = () => {
-    document.location.href = '../home.html'
+
+// function that animate the landing page :
+const LoadingPage = () => {
+    card.classList.add('loaded');
+    presentation.classList.add('appear');
 }
 
 
-// button transtition
+// function that redirect to the home page :
+const redirect = () => {
+    document.location.href = '../home.html'
+}
+// function that animate the transition beetwin the landing page and the home page :
 btn.addEventListener('click', () => {
     btn.classList.add('transition');
     main.classList.add('pageTransition');
-
+    
     setTimeout(() => {
         redirect()
-
     }, 1700)
-
 })
 
 
-// Animation on the load
-window.addEventListener('load', () => {
-    card.classList.add('loaded');
-    presentation.classList.add('appear');
-    btn.classList.add('pageLoad')
-})
+// Animation on the load :
+window.addEventListener('load', LoadingPage())
