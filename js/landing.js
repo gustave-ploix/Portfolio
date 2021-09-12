d8bf697aaf132ec8243a7a41378b367e7a2df37f
gustave ploix <gustaveploix.pro@gmail.com>
2021-09-12 19:30:54 +0200

    finalized the landing page with a page transition

--- js/landing.js
+++ js/landing.js
@@ -1,14 +1,18 @@
+const presentation = document.getElementsByClassName('presentation')[0];
+const card = document.getElementsByClassName('appearCard')[0];
+
 const btn = document.getElementsByClassName('rond')[0];
 
 const main = document.querySelector('main');
-console.log(main);
 
-console.log(btn);
 
+// redirect to the home page
 const redirect = () => {
     document.location.href = '../home.html'
 }
 
+
+// button transtition
 btn.addEventListener('click', () => {
     btn.classList.add('transition');
     main.classList.add('pageTransition');
@@ -18,4 +22,12 @@ btn.addEventListener('click', () => {
 
     }, 1700)
 
+})
+
+
+// Animation on the load
+window.addEventListener('load', () => {
+    card.classList.add('loaded');
+    presentation.classList.add('appear');
+    btn.classList.add('pageLoad')
 })
